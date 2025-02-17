3b394f47a5e2f6f889c4cff2994e4a7bfe71cbb9
namonaki1234 <123581321fns1119@gmail.com>
2025-02-18 04:21:43 +0900

    ✨ 'use client' を各コンポーネントとゲームロジックに追加

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Piece from './Piece';
 import { BoardState } from '../lib/types';
 
--- lib/gameLogic.ts
+++ lib/gameLogic.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { BoardState } from './types';
 
 export const initializeBoard = (): BoardState => {
